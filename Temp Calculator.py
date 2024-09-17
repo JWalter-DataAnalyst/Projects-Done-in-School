a7e62c2657cba80252b9fcd438886707de723972
JWalter-DataAnalyst <jimmywalters11@yahoo.com>
2024-09-17 18:42:28 -0400

    Update Temp Calculator.py

--- Temp Calculator.py
+++ Temp Calculator.py
@@ -1,19 +1,17 @@
-def main():
-    while True:
-        conversion = input("Enter your choice between Fahrenheit or Celsius ").upper()
+while True:
+    conversion = input("Enter your choice between Fahrenheit or Celsius ").upper()
 
-        if conversion == 'F':
-            fahrenheit = float(input("Enter the temperature in Fahrenheit: "))
-            celsius = (fahrenheit - 32) * 5 / 9
-            print("The temperature in Celsius is:", celsius)
+    if conversion == 'F':
+        fahrenheit = float(input("Enter the temperature in Fahrenheit: "))
+        celsius = (fahrenheit - 32) * 5 / 9
+        print("The temperature in Celsius is:", celsius)
 
-        elif conversion == 'C':
-            celsius = float(input("Enter the temperature in Celsius "))
-            fahrenheit = celsius * 9 / 5 + 32
-            print("The temperature in Fahrenheit is:", fahrenheit)
+    elif conversion == 'C':
+        celsius = float(input("Enter the temperature in Celsius "))
+        fahrenheit = celsius * 9 / 5 + 32
+        print("The temperature in Fahrenheit is:", fahrenheit)
         
-        back_to_begining = input("Do you want to perform another conversion? Y/N ").upper()
-        if back_to_begining != "Y":
-            print("Thank you Goodbye. ")
-            break
-main()
+    back_to_begining = input("Do you want to perform another conversion? Y/N ").upper()
+    if back_to_begining != "Y":
+        print("Thank you Goodbye. ")
+        break

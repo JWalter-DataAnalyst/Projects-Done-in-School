fb34ec960b367b2eb3909aa327a07c7138e4b6f2
JWalter-DataAnalyst <jimmywalters11@yahoo.com>
2026-02-10 21:38:33 -0500

    Refactor pizza order logic for clarity and structure
    Signed-off-by: JWalter-DataAnalyst <jimmywalters11@yahoo.com>

--- Pizza order.py
+++ Pizza order.py
@@ -6,27 +6,21 @@ bill = 0
 
 if size == "S":
     bill += 15
-    if pepperoni == "Y":
-        bill += 2
-        print(f"Your bill is {bill}")
-        if extra_cheese == "Y":
-            bill += 1
-            print(f"Your bill is {bill}")
 elif size == "M":
     bill += 20
-    if pepperoni == "Y":
-        bill += 3
-        print(f"Your bill is {bill}")
-        if extra_cheese == "Y":
-            bill += 1
-            print(f"Your bill is {bill}")
 elif size == "L":
     bill += 25
-    if pepperoni == "Y":
-        bill += 3
-        print(f"Your bill is {bill}")
-        if extra_cheese == "Y":
-            bill += 1
-            print(f"Your bill is {bill}")
 else:
-    print("Please select an option listed")
+    print("Have a good day!")
+    exit()
+
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your bill is {bill}")
